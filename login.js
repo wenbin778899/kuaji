823a1203552acf4af3db46d8db708e06198c8c68
wenbin778899 <231275029@smail.nju.edu.cn>
2025-03-30 21:59:32 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 // API基础URL配置
-const API_BASE_URL = 'http://nozomi.proxy.rlwy.net:21839';
+const API_BASE_URL = 'http://nozomi.proxy.rlwy.net:3000';
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
